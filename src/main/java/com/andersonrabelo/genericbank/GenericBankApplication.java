009d178344cd6a3502cf79db66b1b9d2a087a58a
Anderson Rabelo <andersonrabel@gmail.com>
2019-01-01 17:31:38 -0400

    Project created

---
+++ src/main/java/com/andersonrabelo/genericbank/GenericBankApplication.java
@@ -0,0 +1,14 @@
+package com.andersonrabelo.genericbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericBankApplication.class, args);
+	}
+
+}
+
